7e6759e023c925b589a6aac6c9c8dcb13bc637f6
abhm1999 <abdullahmohdi1999@gmail.com>
2023-02-20 01:52:56 +0530

    done

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Loading from "../components/Loading";
 
 
 const Homepage = () => {
      const [data, setData] = useState([]);
      const [loading, setLoading] = useState(false);
+     const [isdata,setIsData]=useState(false)
      const navigate = useNavigate();
 
 
@@ -24,6 +26,7 @@ const Homepage = () => {
              .get("http://localhost:8080/getdata")
              .then((res) => setData(res.data.data));
            setLoading(false);
+           setIsData(true)
          });
        }
      };
@@ -43,6 +46,7 @@ const Homepage = () => {
            .delete("http://localhost:8080/deletedata")
            .then((res) => console.log("Data delete successfull"));
          setData([]);
+         setIsData(false);
        } else {
          // no api will get called
          console.log("Data was not delete from database.");
@@ -63,35 +67,40 @@ const Homepage = () => {
         deletedata
       </button>
       <button onClick={userdetailspage}>User Details </button>
-      <div className="tablediv">
-        <table>
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th colSpan="2">Name</th>
-              <th>Email</th>
-              <th>Country</th>
-              <th>Postcode</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => {
-              return (
-                <tr key={item._id}>
-                  <td>
-                    <img src={item.thumbnail} />
-                  </td>
-                  <td>{item.first}</td>
-                  <td>{item.last}</td>
-                  <td>{item.email}</td>
-                  <td>{item.country}</td>
-                  <td>{item.postcode}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="tablediv">
+          <table>
+            {isdata?<thead>
+              <tr>
+                <th>Image</th>
+                <th colSpan="2">Name</th>
+                <th>Email</th>
+                <th>Country</th>
+                <th>Postcode</th>
+              </tr>
+            </thead>:<></>}
+            
+            <tbody>
+              {data.map((item) => {
+                return (
+                  <tr key={item._id}>
+                    <td>
+                      <img src={item.thumbnail} />
+                    </td>
+                    <td>{item.first}</td>
+                    <td>{item.last}</td>
+                    <td>{item.email}</td>
+                    <td>{item.country}</td>
+                    <td>{item.postcode}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,19 @@
+import { Audio } from "react-loader-spinner";
+function Loading() {
+  return (
+    <div className="loader">
+      <Audio
+        height="150"
+        width="150"
+        radius="20"
+        color="green"
+        ariaLabel="loading"
+
+        //   wrapperStyle
+        //   wrapperClass
+      />
+      <h1>Please Wait </h1>
+    </div>
+  );}
+
+export default Loading;
